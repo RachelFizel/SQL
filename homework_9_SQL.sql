504a5d6a4fd02dc300391645bf4f270b5115c90e
Rachel <rachel.fizel@gmail.com>
2021-03-06 17:44:27 -0600

    clean up

--- homework_9_SQL.sql
+++ homework_9_SQL.sql
@@ -18,22 +18,21 @@ left join departments as d
 on (dm.dept_no = d.dept_no)
 left join employees as e
 on (dm.emp_no = e.emp_no)
-order by dept_no
+order by dept_no;
 
---what does the first part mean?
 --List the department of each employee with the following information: employee number, last name, first name, and department name
 select e.emp_no, e.last_name, e.first_name, d.dept_name
 from employees as e
 join dept_emp as de
 on (e.emp_no = de.emp_no)
 join departments as d
-on (de.dept_no = d.dept_no)
+on (de.dept_no = d.dept_no);
 
 --List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 select first_name, last_name, sex
 from employees
 where first_name = 'Hercules'
-      and last_name like 'B%'
+      and last_name like 'B%';
 	  
 --List all employees in the Sales department, including their employee number, last name, first name, and department name
 select e.emp_no, e.last_name, e.first_name, d.dept_name
@@ -42,7 +41,7 @@ join dept_emp as de
 on (e.emp_no = de.emp_no)
 join departments as d
 on (de.dept_no = d.dept_no)
-where dept_name = 'Sales'
+where dept_name = 'Sales';
 
 --List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name
 select e.emp_no, e.last_name, e.first_name, d.dept_name
@@ -52,7 +51,7 @@ on (e.emp_no = de.emp_no)
 join departments as d
 on (de.dept_no = d.dept_no)
 where dept_name = 'Sales'
-      or dept_name = 'Development'
+      or dept_name = 'Development';
 
 --In descending order, list the frequency count of employee last names, i.e., how many employees share each last name
 select last_name, count(*)
